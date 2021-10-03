d3c1a4911c595fbb644e8f602c1b7ffda6e2a712
Vipul <Vipulsoniab17@gmail.com>
2021-10-03 10:47:15 +0530

    adding booking and travellers page

--- frontend/ease-my-trip/src/App.js
+++ frontend/ease-my-trip/src/App.js
@@ -2,11 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { Flights } from "./Components/Flights";
+import BookingPage from "./Components/BookingPage";
+import Travellers from "./Components/Travellers";
 
 function App() {
   return (
     <div className="App">
-      <Flights />
+      {/* <Flights /> */}
+      {/* <BookingPage /> */}
+      <Travellers />
     </div>
   );
 }
