c6d644e3d6caa43f2847a1bbf0cabcaff28822e3
Niket Nayan <82999578+Niket1-1@users.noreply.github.com>
2021-10-03 09:27:26 +0530

    Update Flights.jsx

--- frontend/ease-my-trip/src/Components/Flights.jsx
+++ frontend/ease-my-trip/src/Components/Flights.jsx
@@ -1,15 +1,1 @@
-import React from "react";
-import { Footer } from "./Footer";
-import { Navbar } from "./Navbar";
 
-const Flights = () => {
-  return (
-    <>
-      <Navbar />
-      <h1>Flights page</h1>
-      <Footer />
-    </>
-  );
-};
-
-export { Flights };
