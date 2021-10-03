0213539c288fa72c52f4d8e019f7c2089a2fbf91
Niket Nayan <82999578+Niket1-1@users.noreply.github.com>
2021-10-03 09:19:15 +0530

    Update App.js

--- frontend/ease-my-trip/src/App.js
+++ frontend/ease-my-trip/src/App.js
@@ -1,14 +1,1 @@
-// import logo from "./logo.svg";
-// import "./App.css";
 
-import { Flights } from "./Components/Flights";
-
-function App() {
-  return (
-    <div className="App">
-      <Flights />
-    </div>
-  );
-}
-
-export default App;
