5d230908176fcc408771125aabfd4c01c1567292
Niket Nayan <82999578+Niket1-1@users.noreply.github.com>
2021-10-03 11:29:56 +0530

    Update App.js

--- frontend/ease-my-trip/src/App.js
+++ frontend/ease-my-trip/src/App.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
-import "./App.css";
 
-import { Flights } from "./Components/Flights";
-import BookingPage from "./Components/BookingPage";
-import Travellers from "./Components/Travellers";
-
-function App() {
-  return (
-    <div className="App">
-      {/* <Flights /> */}
-      {/* <BookingPage /> */}
-      <Travellers />
-    </div>
-=======
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./Components/Route";
 
@@ -26,7 +11,7 @@ function App() {
         </div>
       </BrowserRouter>
     </>
->>>>>>> 772bc08431f294685d791e9269e74cedb17598db
+
   );
 }
 
